e84229320ec664284811e9b256225c205b20a24d
ahmadiga <abazadough@aswaq.com>
2018-04-22 11:07:33 +0300

    update version

--- sample/sample.py
+++ sample/sample.py
@@ -6,8 +6,10 @@ notification = client.send_notifications(
     title="title",
     message_data='{"url": "","body": {"action": "product","type": 2,"value": {"type": "product","value": "3113"}},"message": "hi"}',
     android_user_tokens=[
-        "ftq_18WNn-Y:APA91bHs9m3fcLZlxPTnhJ7sNKiWZ0dS5szjFj-omYCaBZsrjvZLyhAENO2DZ8GkeoSzLLCXwNGRpJCVVU95MH-rPeaVMtI18ihCAUmSyG5TeWgnpai0rGpApRI8T6jj38QGqqqVinnK"],
+        "eC9tL3yNf08:APA91bHf86EsUNaWeIDBC-4ki_MC95iGWWJRVhfIaWs4d_W6Kn4eWALTqvBhPOpDDbtowZgWMs3UWUUVmjkHqRRkYtgKdM4Z-3DFKDm7bPjTA-viHT8aMdoonjlLBGh1XBDPy0mpIf2K",
+    ],
     ios_user_tokens=[
-        "eC9tL3yNf08:APA91bHf86EsUNaWeIDBC-4ki_MC95iGWWJRVhfIaWs4d_W6Kn4eWALTqvBhPOpDDbtowZgWMs3UWUUVmjkHqRRkYtgKdM4Z-3DFKDm7bPjTA-viHT8aMdoonjlLBGh1XBDPy0mpIf2K"]
+        "ftq_18WNn-Y:APA91bHs9m3fcLZlxPTnhJ7sNKiWZ0dS5szjFj-omYCaBZsrjvZLyhAENO2DZ8GkeoSzLLCXwNGRpJCVVU95MH-rPeaVMtI18ihCAUmSyG5TeWgnpai0rGpApRI8T6jj38QGqqqVinnK",
+    ]
 )
 print(notification)
